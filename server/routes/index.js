963c48457a1c82b1fa269b3a95ba3df459f75f51
stepforwardtech <jrstrayhorn@gmail.com>
2017-01-05 10:50:09 -0500

    finished server side for login functionality

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,6 +1,11 @@
 var express = require('express');
 var router = express.Router();
 
+var auth = require('../../server/config/auth');
+
+/* authentication routes */
+router.post('/login', auth.authenticate);
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
