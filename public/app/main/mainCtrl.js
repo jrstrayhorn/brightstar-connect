35b2010a294a838054c2c0dc8456d78539773cd8
stepforwardtech <jrstrayhorn@gmail.com>
2017-01-10 10:42:09 -0500

    updating main page content

--- public/app/main/mainCtrl.js
+++ public/app/main/mainCtrl.js
@@ -1,14 +1,15 @@
 angular.module('app').controller('mainCtrl', ['$scope', function($scope) {
     $scope.technologies = [
-        {name: 'MEAN Stack - MongoDB, Express 4, Angular 1.5, Node'},
-        {name: 'UI Router'},
-        {name: 'Toastr'},
-        {name: 'jQuery'},
-        {name: 'express-generator'},
-        {name: 'mongoose'},
-        {name: 'passport'},
-        {name: 'JSON Web Tokens'},
-        {name: 'express-jwt'},
-        {name: 'nodemailer'}
+        {name: 'MEAN Stack - MongoDB, Express 4, Angular 1.5, Node.js'},
+        {name: 'UI Router - flexible routing in AngularJS'},
+        {name: 'Toastr - JS library for non-blocking notifications'},
+        {name: 'express-generator - quickly generate express application skeleton'},
+        {name: 'mongoose - elegant mongodb object modeling for node.js'},
+        {name: 'passport - authentication middleware for node.js'},
+        {name: 'JSON Web Tokens - assisting with security in application'},
+        {name: 'express-jwt - middleware that validates JsonWebtokens'},
+        {name: 'nodemailer - send e-mails with Node.js'},
+        {name: 'Heroku - cloud app hosting (Platform as a Service)'},
+        {name: 'mLab - cloud hosted MongoDB (Database as a Service)'}
     ];
 }]);
